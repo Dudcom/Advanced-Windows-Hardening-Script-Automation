3a6d6b3824cc3c4a8028e5c1700a0f01bc1d04f4
Dudcom <70006792+Dudcom@users.noreply.github.com>
2024-09-03 12:17:09 -0400

    More Troy Teaching Material + some cleaning up
    Cisco, Windows and Linux are all here !

--- more random crap idk/Cyber Dump/Windows_Unit_3_Lab_Summary/Cyber Scripts heh/anti-scorpio/resetGPO.bat
+++
@@ -1,4 +0,0 @@
-del C:\Windows\System32\GroupPolicy\Machine\Registry.pol /f
-del C:\Windows\System32\GroupPolicy\User\Registry.pol /f
-gpupdate /force
-pause
--- more random crap idk/Cyber Dump/Windows_Unit_3_Lab_Summary/Cyber Scripts heh/anti-scorpio/revertLSP.bat
+++
@@ -1,2 +0,0 @@
-secedit /configure /db temp.sdb /cfg %userprofile%\Desktop\ScriptOut\lspbackup.inf
-pause
